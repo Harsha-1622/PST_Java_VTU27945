9d808658d158924a899a38d4e9f40bb6cbb963fd
B.Sai Sree Harsha <harshabhogaraju@gmail.com>
2026-07-23 23:27:09 +0530

    Add palindrome checker with functional interface
    Implement palindrome checking using a functional interface.

---
+++ PST_Java Task 9 VTU27945.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+@FunctionalInterface
+interface Palindrome {
+    boolean check(int n);
+}
+
+public class Main {
+
+    public static boolean isPalindrome(int n) {
+        int original = n;
+        int reverse = 0;
+
+        while (n != 0) {
+            int digit = n % 10;
+            reverse = reverse * 10 + digit;
+            n /= 10;
+        }
+
+        return original == reverse;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        // Method Reference
+        Palindrome p = Main::isPalindrome;
+
+        if (p.check(num))
+            System.out.println(num + " is a Palindrome.");
+        else
+            System.out.println(num + " is not a Palindrome.");
+
+        sc.close();
+    }
+
+  Output :
+  Enter a number: 55
+  55 is a Palindrome.
+}
