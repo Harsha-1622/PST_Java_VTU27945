f917e819005fc37e946d4697ca19f42eb1d17299
B.Sai Sree Harsha <harshabhogaraju@gmail.com>
2026-08-20 10:26:31 +0530

    Update Session 13 Java ArrayList VTU 27945.java

--- Week 3/Session 13 Java ArrayList VTU 27945.java
+++ Week 3/Session 13 Java ArrayList VTU 27945.java
@@ -49,4 +49,24 @@ public class Solution {
         sc.close();
     }
 }
-    
+
+
+Input :
+12
+ENTER John 3.75 50
+ENTER Mark 3.8 24
+ENTER Shafaet 3.7 35
+SERVED
+SERVED
+ENTER Samiha 3.85 36
+SERVED
+ENTER Ashley 3.9 42
+ENTER Maria 3.6 46
+ENTER Anik 3.95 49
+ENTER Dan 3.95 50
+SERVED 
+Output :
+Dan
+Ashley
+Shafaet
+Maria
