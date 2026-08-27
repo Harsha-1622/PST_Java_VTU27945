629495f6440a23933dcec669c42717630783e388
B.Sai Sree Harsha <harshabhogaraju@gmail.com>
2026-08-27 09:10:18 +0530

    Fix formatting and add sample input/output

--- Week 1/Session 3 Sorting Comparator VTU 27945.java
+++ Week 1/Session 3 Sorting Comparator VTU 27945.java
@@ -44,3 +44,19 @@ public class Solution {
         }
     }
 }
+
+Input :
+
+5
+amy 100
+david 100
+heraldo 50
+aakansha 75
+aleksa 150
+Output :
+
+aleksa 150
+amy 100
+david 100
+aakansha 75
+heraldo 50
