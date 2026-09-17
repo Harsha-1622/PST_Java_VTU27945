9c81a7f6310578044fd89359dad16d83b5c0cd28
B.Sai Sree Harsha <harshabhogaraju@gmail.com>
2026-09-17 10:32:10 +0530

    Create Session 2 Task 3 VTU 27945.java

---
+++ Week 8/Session 2 Task 3 VTU 27945.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.lang.reflect.*;
+
+class Singleton {
+
+    // Single instance
+    private static Singleton instance = new Singleton();
+
+    // Public String variable
+    public String str;
+
+    // Private constructor
+    private Singleton() {
+    }
+
+    // Return the single instance
+    public static Singleton getSingleInstance() {
+        return instance;
+    }
+}
+
+public class Main {
+
+    public static void main(String args[]) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+
+        Singleton s1 = Singleton.getSingleInstance();
+        Singleton s2 = Singleton.getSingleInstance();
+
+        assert(s1 == s2);
+
+        // Verify that constructor is private
+        Class c = s1.getClass();
+        Constructor[] allConstructors = c.getDeclaredConstructors();
+
+        assert allConstructors.length == 1;
+
+        for (Constructor ctor : allConstructors) {
+            if (ctor.getModifiers() != 2 ||
+                !ctor.toString().equals("private Singleton()")) {
+                System.out.println("Wrong class!");
+            }
+        }
+
+        String str = sc.nextLine();
+
+        s1.str = str;
+        s2.str = str;
+
+        System.out.println(
+            "Hello I am a singleton! Let me say " + str + " to you"
+        );
+    }
+}
+
+Input :
+hello world
+Output :
+Hello I am a singleton! Let me say hello world to you
